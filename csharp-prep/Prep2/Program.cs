b9878fa89e6cb04aa174dfec2956205100c9ecf0
Xana Green <byuixana@byui.edu>
2023-09-15 17:21:28 -0600

    Finished program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int grade_percent = int.Parse(grade);
+        string letter = "A";
+        if (grade_percent >= 90)
+        {
+            Console.WriteLine(letter);
+        }
+        else if (grade_percent >= 80)
+        {
+            letter = "B";
+            Console.WriteLine(letter);
+        }
+        else if (grade_percent >= 70)
+        {
+            letter = "C";
+            Console.WriteLine(letter);
+        }
+        else if (grade_percent >= 60)
+        {
+            letter = "D";
+            Console.WriteLine(letter);
+        }
+        else if (grade_percent >= 50)
+        {
+            letter = "F";
+            Console.WriteLine(letter);
+        }
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("You passed! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
