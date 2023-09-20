21e21366026fe88bc07ab3da8a7c496a192f451b
Xana Green <byuixana@byui.edu>
2023-09-20 14:45:54 -0600

    Fixed Prep3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
@@ -16,22 +19,42 @@ class Program
         // Asks users for a number.
         Console.Write("Enter a number:");
         string number_string = Console.ReadLine();
+
         int int_number = int.Parse(number_string);
+
         int sum = 0;
+        
+        int largest_num = 0;
+  
         while (int_number != 0)
             {
                 // Adds numbers to list.
                 numbers.Add(int_number);
+
                 Console.Write("Enter a number:");
+
                 number_string = Console.ReadLine();
+
                 int_number = int.Parse(number_string);
                 
                 
             }
         foreach (int number in numbers)
                 {
-                    sum += number;      
+                    sum += number;   
+
+                    if (number > largest_num)
+                        largest_num = number;  
                 }
+
+        //Should have added a (float) by sum to get a decimal number instead of integers.
+        int average = sum / numbers.Count();
+
         Console.WriteLine(sum);
+
+        Console.WriteLine(average);
+
+        Console.WriteLine(largest_num);
+
     }
 }
