e223e87364ed685c11b82443ce58294323e77d61
Xana Green <byuixana@byui.edu>
2023-09-20 09:18:22 -0600

    Prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        string magic_number_str = Console.ReadLine();
+        int magic_number = int.Parse(magic_number_str);
+        Console.Write("What is your guess?");
+        string guess_string = Console.ReadLine();
+        int guess_number = int.Parse(guess_string);
+        while (guess_number != magic_number)
+            // An if statement that checks if the guess number is high or lower
+            {
+            if (guess_number < magic_number)
+                Console.WriteLine("Higher!");
+            if (guess_number > magic_number)
+                Console.WriteLine("Lower!");
+            Console.Write("What's your new guess?");
+            guess_string = Console.ReadLine();
+            guess_number = int.Parse(guess_string);
+            }
+        Console.WriteLine("You guessed correctly! Good work!");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,58 @@
 using System;
-
+ // Examples of C# functions:
+        // returnType FunctionName(dataType parameter1, dataType parameter2)
+            // {
+            //     // function_body
+            // }
+            
+        // int AddNumbers(int first, int second)
+        // {
+        //     int sum = first + second;
+        //     return sum;
+        // }
+        // void DisplayPersonalMessage(string user_name):
+        //     Console.WriteLine($"Hello {user_name}")
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        static string Main();
+            {
+            }
+                //Welcomes user with their name
+                static void DisplayWelcome();
+                    {
+                        Console.WriteLine("Welcome to the program!");
+                    }
+
+                 // Asks for and converts username into a string
+                static string PromptUserName();
+                    {
+                        Console.Write("Please enter your name:");
+                        string user_name = Console.ReadLine();
+                        return user_name;
+                    }
+
+                static int PromptUserNumber();
+                    {
+                        Console.Write("Please enter your number:");
+                        string user_number_str = Console.ReadLine();
+                        int user_number = int.Parse(user_number_string);
+                        return user_number;
+                    }
+                    
+                static int SquareNumber(int user_number);
+                    {
+                        int square_number = user_number**2;
+                        return squared_number;
+                    }
+                static string DisplayResult(string user_name, int squared_number);
+                    {
+                        string result = Console.WriteLine($"{user_name}, the square of your favorite number is {squared_number}.")
+                    }
+        Main();
+
     }
 }
